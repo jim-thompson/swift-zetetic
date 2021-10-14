468ef336b30faa29da80fe439e91170a5a150044
Jim Thompson <jim.thompson@pobox.com>
2021-10-13 19:20:53 -0500

    First version working

--- slider-test/slider-test/ContentView.swift
+++ slider-test/slider-test/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var speed = 50.0
+    @State private var isEditing = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Slider(value: $speed,
+                   in: 0...100, step: 0.1,
+                   onEditingChanged: { editing in
+                isEditing = editing
+            })
+                .padding()
+            Text("\(speedDisplay)")
+                .foregroundColor(speed > 50 ? .green : .blue)
+        }
+    }
+    
+    var speedDisplay: String {
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 1
+        formatter.maximumFractionDigits = 1
+
+        return formatter.string(from: NSNumber(value: speed)) ?? "?"
+//        return "?"
     }
 }
 
